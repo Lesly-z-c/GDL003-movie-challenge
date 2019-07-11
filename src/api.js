7a9d20d0760ff9358b8b4c7b0275d13c5e0f59cd
Brenda Vielmas <breen.vielmas@gmail.com>
2019-07-11 11:00:36 -0500

    Inicio de funciones

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
 window.api = {
-    findMovies : (data, showMoviesOfActor) => {
-
-
+    findMovies : (search, page) => {
+        const movies = `http://www.omdbapi.com/?s=${search}&type=movie&page=${page}&apikey=31a448c5`;
+        fetch(movies)
+        .then( response => response.json() )
+        .then( data => console.log(data) )
     }
+    
+         window.api.findMovies('terror',1);
+
 };
