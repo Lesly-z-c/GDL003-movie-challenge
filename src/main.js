b3c85eb89e597250405b6254a6ad6586ce2c5cd6
Silvana <silvana15_91@hotmail.com>
2019-07-11 22:14:49 -0500

    Se agrego tarjeta y funcionalidad de botones

--- src/main.js
+++ src/main.js
@@ -1,36 +1,41 @@
-document.getElementById("homePage").style.display = "block";
-document.getElementById("btnOfPage1").style.display = "none";
-document.getElementById("btnOfPage2").style.display = "none";
-document.getElementById("btnOfPage3").style.display = "none";
+let boton_busqueda = document.getElementById('Busqueda');
+let boton_previous = document.getElementById('boton_prev');
+let boton_next = document.getElementById('boton_next');
+let resultado = document.getElementById('resultado');
+let texto = document.getElementById('buscar-texto');
+let current_page = 1;
 
-let showPagesOfSeasons = () => {
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("btnOfSeasons").style.display = "none";
-    document.getElementById("btnOfPage1").style.display = "block";
-    document.getElementById("btnOfPage2").style.display = "block";
-    document.getElementById("btnOfPage3").style.display = "block";
-    document.getElementById("btnOfCharacters").style.display = "none";
-}
+boton_busqueda.addEventListener('click',()=> {
+    let busqueda = texto.value;
+    //promesa que regresa el fetch
+    window.api.findMovies(busqueda, current_page).then( movies => {
+        movies.forEach(movie => {
+          let card = print_movie(movie);
+          resultado.innerHTML += card;
+        });
+    });
+});
 
-let showCharacters = () => {
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("btnOfSeasons").style.display = "none";
-    document.getElementById("btnOfCharacters").style.display = "none";
-}
+boton_previous.addEventListener('click',()=> {
+    let busqueda = texto.value;
+    //promesa que regresa el fetch
+    current_page = current_page -1;
+    window.api.findMovies(busqueda, current_page).then( movies => {
+        movies.forEach(movie => {
+          let card = print_movie(movie);
+          resultado.innerHTML += card;
+        });
+    });
+});
 
-
-function print_character(Search) {
-
-    return '<div class="card">' +
-                `<img src="${Search.Poster}">` +
-                '<div class="container">' +
-                    `<h4>Name: ${Search.Title}</h4>` +
-                    `<p>Status: ${Search.Year}</p>` +
-                '</div>' +
-            '</div>';
- 
- }
-
-document.getElementById("btnOfSeasons").addEventListener("click",showPagesOfSeasons);
-document.getElementById("btnOfCharacters").addEventListener("click",showCharacters);
-
+boton_next.addEventListener('click',()=> {
+    let busqueda = texto.value;
+    //promesa que regresa el fetch
+    current_page = current_page +1;
+    window.api.findMovies(busqueda, current_page).then( movies => {
+        movies.forEach(movie => {
+          let card = print_movie(movie);
+          resultado.innerHTML += card;
+        });
+    });
+});
